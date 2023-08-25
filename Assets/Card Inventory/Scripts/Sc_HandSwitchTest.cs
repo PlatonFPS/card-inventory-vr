4625d014e3b93f538b44c66ebaf724366c8ea0f5
PlatonFPS <75620080+PlatonFPS@users.noreply.github.com>
2023-08-25 18:02:16 +0300

    Started to work on feature to keep hands where they need to be

---
+++ Assets/Card Inventory/Scripts/Sc_HandSwitchTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Sc_HandSwitchTest : MonoBehaviour
+{
+    private XRGrabInteractable interactable;
+
+    private void Awake()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+        interactable.selectEntered.AddListener(Pon);
+    }
+    private void Pon(SelectEnterEventArgs arg0)
+    {
+        Debug.Log(arg0.interactorObject.transform.name);
+    }
+
+    private Transform temporaryTransform;
+    private void SwitchAttachTransforms()
+    {
+        temporaryTransform = interactable.attachTransform;
+        interactable.attachTransform = interactable.secondaryAttachTransform;
+        interactable.secondaryAttachTransform = temporaryTransform;
+    }
+    
+}
